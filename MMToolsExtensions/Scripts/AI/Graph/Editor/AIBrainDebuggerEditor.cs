af3619b65236f6448269ba988c9275f0909dbbd1
Marco Secchi <loziosecchi@gmail.com>
2019-11-18 10:22:35 +0100

    Add debugger window and corresponding events.

---
+++ MMToolsExtensions/Scripts/AI/Graph/Editor/AIBrainDebuggerEditor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace TheBitCave.MMToolsExtensions.AI.Graph
+{
+
+    public class AIBrainDebuggerEditor : EditorWindow
+    {
+        [MenuItem("Tools/TheBitCave/MM Extensions/AI Brain Debugger")]
+        private static void Init()
+        {
+            var window = GetWindow<AIBrainDebuggerEditor>("AI Brain Debugger", true);
+            window.Show();
+        }
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnInspectorUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnGUI()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
