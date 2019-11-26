e7a5ec68428531ce46db2181d5f67f34d5ce1b1d
Marco Secchi <loziosecchi@gmail.com>
2019-11-26 15:02:15 +0100

    Fix error ID#3.

---
+++ MMToolsExtensions/Scripts/AI/Graph/Editor/AIBrainPluggableEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+using UnityEditor;
+
+namespace TheBitCave.MMToolsExtensions.AI.Graph
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(AIBrainPluggable))]
+    public class AIBrainPluggableEditor : AIBrainEditor
+    {
+    }
+}
