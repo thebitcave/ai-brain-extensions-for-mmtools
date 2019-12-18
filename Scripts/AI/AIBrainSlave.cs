7f483d55e0df9d2125a475d15754b01b5693c113
Marco Secchi <loziosecchi@gmail.com>
2019-12-18 11:38:47 +0100

    Add master/slave feature.

---
+++ Scripts/AI/AIBrainSlave.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.Tools;
+using UnityEngine;
+
+namespace TheBitCave.MMToolsExtensions.AI
+{
+    /// <summary>
+    /// A component used to control the Corgi <see cref="MoreMountains.Tools.AIBrain"/> as a slave from
+    /// another (master) AIBrain. 
+    /// </summary>
+    [RequireComponent(typeof(AIBrain))]
+    public class AIBrainSlave : MonoBehaviour, MMEventListener<AIBrainChangeStateCommandEvent>
+    {
+        // An identifier used to filter commands from the master slave (if the channel name corresponds, the brain will transition to the desired state).
+        public string ChannelName;
+        
+        private AIBrain _aiBrain;
+        
+        private void Start()
+        {
+            _aiBrain = GetComponent<AIBrain>();
+        }
+
+        /// <summary>
+        /// If the channel sent by the command corresponds and the state exists,
+        /// forces state transition.
+        /// </summary>
+        /// <param name="aiBrainChangeStateCommandEvent"></param>
+        public virtual void OnMMEvent(AIBrainChangeStateCommandEvent aiBrainChangeStateCommandEvent)
+        {
+            if (aiBrainChangeStateCommandEvent.ChannelName != ChannelName) return;
+            var hasState = _aiBrain.States.Any(state => state.StateName == aiBrainChangeStateCommandEvent.StateName);
+            if (!hasState) return;
+            _aiBrain.Target = aiBrainChangeStateCommandEvent.Target;
+            _aiBrain.TransitionToState(aiBrainChangeStateCommandEvent.StateName);
+        }
+        
+        private void OnEnable()
+        {
+            this.MMEventStartListening<AIBrainChangeStateCommandEvent>();
+        }
+        
+        private void OnDisable()
+        {
+            this.MMEventStopListening<AIBrainChangeStateCommandEvent>();
+        }
+    }
+}
---
+++ Scripts/Events/AIBrainChangeStateCommandEvent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace TheBitCave.MMToolsExtensions.Tools
+{
+    /// <summary>
+    /// Events used to control AIBrains
+    /// </summar>
+    public struct AIBrainChangeStateCommandEvent
+    {
+        // A name used to filter commands from the slave
+        public string ChannelName;
+        
+        // The State the slave brain should enter in
+        public string StateName;
+        
+        // The brain target, if any
+        public Transform Target;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="channelName">A name used to filter commands from the slave.</param>
+        /// <param name="stateName">The State the slave brain should enter in.</param>
+        /// <param name="target">// The brain target, if any.</param>
+        public AIBrainChangeStateCommandEvent(string channelName, string stateName, Transform target)
+        {
+            ChannelName = channelName;
+            StateName = stateName;
+            Target = target;
+        }
+    }
+}
