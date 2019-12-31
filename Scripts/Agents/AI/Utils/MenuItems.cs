1670f0fe04d8b9c041e9f0cef8012e4f0876141a
marcosecchi <loziosecchi@gmail.com>
2019-12-31 10:22:07 +0100

    Add utility method to remove the AI System.

---
+++ Scripts/Agents/AI/Utils/MenuItems.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace TheBitCave.MMToolsExtensions.AI.Graph
+{
+    public static class MenuItems
+    {
+        /// <summary>
+        /// Removes all the AI System (AIBrain, AIActions and AIDecisions)
+        /// </summary>
+        [MenuItem("GameObject/AI Brain/Remove AI Brain System", false, 100)]
+        private static void RemoveAIBrainSystem()
+        {
+            if (Selection.activeGameObject == null) return;
+            GeneratorUtils.Cleanup(Selection.activeGameObject);
+        }
+    }
+}
